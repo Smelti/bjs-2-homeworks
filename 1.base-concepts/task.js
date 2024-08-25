8f71419d0b32b1b1e1dbad745125be3705c08231
Smelti <164363091+Smelti@users.noreply.github.com>
2024-08-25 17:47:18 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = (b*b) - (4 * a * c);
+  let disk = (b*b) - (4 * a * c);
   if (d < 0) {
     arr = [];
   }
-  else if (task === 0) {
+  else if (disk === 0) {
     arr = [-b / (2 * a)];
   }
-  else if (task > 0) {
+  else if (disk > 0) {
     arr = [(-b + Math.sqrt(d)) / (2 * a),
        (-b - Math.sqrt(d)) / (2 * a)];
   }
