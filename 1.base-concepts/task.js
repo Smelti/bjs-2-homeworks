bb5b8ab55af9ee6d453f0cfdad565a5dd37efd0a
Smelti <164363091+Smelti@users.noreply.github.com>
2024-08-25 17:52:01 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,23 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let disk = (b*b) - (4 * a * c);
+  let d = (b*b) - (4 * a * c);
   if (d < 0) {
     arr = [];
   }
-  else if (disk === 0) {
+  if (d === 0) {
     arr = [-b / (2 * a)];
   }
-  else if (disk > 0) {
+  if (d > 0) {
     arr = [(-b + Math.sqrt(d)) / (2 * a),
        (-b - Math.sqrt(d)) / (2 * a)];
   }
   return arr;
 }
 
-// function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-//   let credit = amount - contribution;
-//   let interestRate = percent / 100 / 12;
-//   let monthlyPayment = credit * (interestRate + (interestRate / (((1 + interestRate)**countMonths) - 1)));
-//   return Math.cell(monthlyPayment).toFixed(2);
-// }
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let credit = amount - contribution;
+  let interestRate = percent / 100 / 12;
+  let monthlyPayment = credit * (interestRate + (interestRate / (((1 + interestRate)**countMonths) - 1)));
+  return Math.cell(monthlyPayment).toFixed(2);
+}
