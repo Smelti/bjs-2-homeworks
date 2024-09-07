1ee22ad6604e2897e828d5809b372b13d192b254
Smelti <164363091+Smelti@users.noreply.github.com>
2024-09-07 17:30:26 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,69 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = arr.reduce((a, b) => (a + b)) / arr.length;
+  let avg = Number(sum.toFixed(2));
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max === arr[i];
+    }
+    if (arr[i] < min) {
+      min === arr[i];
+    }
+  }
+  return {min: min, max: max, avg: avg};
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let difference = max - min;
+  return difference;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEvenElemets = 0;
+  let sumOddElements = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElemets += arr[i];
+    }
+    if (arr[i] % 2 !== 0) {
+      sumOddElements += arr[i];
+    }
+  }
+  let difElements = sumEvenElemets - sumOddElements;
+  return difElements;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let SumElemets = 0;
+  let countElements = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      SumElemets += arr[i];
+      countElements++;
+    }
+  }
+  let avg = SumElemets / countElements;
+  return avg;
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const result = func(arrOfArr[i])
+    if (result > maxWorkerResult) {
+      maxWorkerResult = result;
+    }
+  }
+  return maxWorkerResult;
 }
