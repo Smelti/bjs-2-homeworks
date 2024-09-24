9da0d13df74ad4d17ae01495e50ac6a7ee068b3c
Smelti <164363091+Smelti@users.noreply.github.com>
2024-09-24 19:51:12 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks.length === 0) {
+    this.marks = [...marksToAdd]
+  } else {
+    this.marks = [...this.marks, ...marksToAdd]
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks.length === 0) {
+    return 0
+  }
+  return this.marks.reduce((acc, marks) => acc + marks, 0) / this.marks.length
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  let excluded = new Object(reason);
+  this.excluded = reason;
 }
