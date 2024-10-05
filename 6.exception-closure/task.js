32321733012e11a1e13d94c5969c041daaeea558
Smelti <164363091+Smelti@users.noreply.github.com>
2024-10-05 14:47:47 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,14 @@
-﻿
+﻿function parseCount(count) {
+  if (Number.isNaN(count) || Number.parseFloat(count)) {
+    throw new Error("Невалидное значение")
+  }
+  return count
+}
+
+function validateCount(count) {
+  try {
+    return parseCount(count)
+  } catch (error) {
+    return error
+  }
+}
