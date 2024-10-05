c5572d9a2dd822ddb227dc79cfb4bc69ad935e54
Smelti <164363091+Smelti@users.noreply.github.com>
2024-10-05 15:10:39 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -13,3 +13,36 @@ function validateCount(count) {
     return error
   }
 }
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (this.a + b < this.c || this.a + this.c < b || this.b + this.c < this.a) {
+      throw new Error ("Треугольник с такими сторонами не существует")
+    }  
+  }
+  get perimeter() {
+    return this.a + this.b + this.c
+  }
+  get area() {
+    const p = this.perimeter / 2
+    return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c)
+  } catch(error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует"
+      },
+      get peimeter() {
+        return "Ошибка! Треугольник не существует"
+      }
+    }
+  }
+}
